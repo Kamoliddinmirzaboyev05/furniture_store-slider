425e02365d407a461df77b39fa822ab27c8fbbd9
Kamoliddinmirzaboyev05 <kamoliddinmirzaboyev2005@gmail.com>
2024-12-17 11:33:43 +0500

    Slider add

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const minusBtn = document.querySelector(".minus"),
   plusBtn = document.querySelector(".plus"),
   number = document.querySelector(".num"),
-  img2 = document.querySelector("#item1"),
-  img3 = document.querySelector("#item2"),
-  img4 = document.querySelector("#item3"),
-  img5 = document.querySelector("#item4"),
-  img6 = document.querySelector("#item5"),
+  img1 = document.querySelector("#item1"),
+  img2 = document.querySelector("#item2"),
+  img3 = document.querySelector("#item3"),
+  img4 = document.querySelector("#item4"),
+  img5 = document.querySelector("#item5"),
+  left = document.querySelector("#left"),
+  right = document.querySelector("#right"),
   orderNum = document.querySelector("#orderNum"),
   mainImg = document.querySelector(".main-img");
-
+var imgIndex = 1;
 plusBtn.addEventListener("click", () => {
   var value = Number(number.textContent);
   number.textContent = value + 1;
@@ -21,48 +23,80 @@ minusBtn.addEventListener("click", () => {
   }
 });
 
-img2.addEventListener("click", () => {
-    orderNum.textContent = '01'
-  item1.style.border = "2px solid green";
-  item2.style.border = "2px solid #a2a3b1";
-  item3.style.border = "2px solid #a2a3b1";
-  item4.style.border = "2px solid #a2a3b1";
-  item5.style.border = "2px solid #a2a3b1";
+left.addEventListener("click", () => {
+  if (imgIndex == 1) {
+    img5.click();
+  } else if (imgIndex == 5) {
+    img4.click();
+  } else if (imgIndex == 4) {
+    img3.click();
+  } else if (imgIndex == 3) {
+    img2.click();
+  } else if (imgIndex == 2) {
+    img1.click();
+  }
+});
+right.addEventListener("click", () => {
+  if (imgIndex == 1) {
+    img2.click();
+  } else if (imgIndex == 2) {
+    img3.click();
+  } else if (imgIndex == 3) {
+    img4.click();
+  } else if (imgIndex == 4) {
+    img5.click();
+  } else if (imgIndex == 5) {
+    img1.click();
+  }
+});
+img1.addEventListener("click", () => {
+  imgIndex = 1;
+  orderNum.textContent = "01";
+  img1.classList.add("active");
+  img2.classList.remove("active");
+  img3.classList.remove("active");
+  img4.classList.remove("active");
+  img5.classList.remove("active");
   mainImg.innerHTML = `<img src="img/img2.svg" alt="" />`;
 });
-img3.addEventListener("click", () => {
-    orderNum.textContent = '02'
-  item1.style.border = "2px solid #a2a3b1";
-  item3.style.border = "2px solid #a2a3b1";
-  item4.style.border = "2px solid #a2a3b1";
-  item5.style.border = "2px solid #a2a3b1";
-  item2.style.border = "2px solid green";
+img2.addEventListener("click", () => {
+  imgIndex = 2;
+  img2.classList.add("active");
+  img1.classList.remove("active");
+  img3.classList.remove("active");
+  img4.classList.remove("active");
+  img5.classList.remove("active");
+  orderNum.textContent = "02";
   mainImg.innerHTML = `<img src="img/img3.svg" alt="" />`;
 });
-img4.addEventListener("click", () => {
-    orderNum.textContent = '03'
-  item1.style.border = "2px solid #a2a3b1";
-  item2.style.border = "2px solid #a2a3b1";
-  item4.style.border = "2px solid #a2a3b1";
-  item5.style.border = "2px solid #a2a3b1";
-  item3.style.border = "2px solid green";
+img3.addEventListener("click", () => {
+  imgIndex = 3;
+  img3.classList.add("active");
+  img2.classList.remove("active");
+  img1.classList.remove("active");
+  img4.classList.remove("active");
+  img5.classList.remove("active");
+  orderNum.textContent = "03";
   mainImg.innerHTML = `<img src="img/img4.svg" alt="" />`;
 });
-img5.addEventListener("click", () => {
-    orderNum.textContent = '04'
-  item1.style.border = "2px solid #a2a3b1";
-  item2.style.border = "2px solid #a2a3b1";
-  item3.style.border = "2px solid #a2a3b1";
-  item5.style.border = "2px solid #a2a3b1";
-  item4.style.border = "2px solid green";
+img4.addEventListener("click", () => {
+  imgIndex = 4;
+  orderNum.textContent = "04";
+  img4.classList.add("active");
+  img2.classList.remove("active");
+  img3.classList.remove("active");
+  img1.classList.remove("active");
+  img5.classList.remove("active");
+
   mainImg.innerHTML = `<img src="img/img5.svg" alt="" />`;
 });
-img6.addEventListener("click", () => {
-    orderNum.textContent = '05'
-  item5.style.border = "2px solid green";
-  item1.style.border = "2px solid #a2a3b1";
-  item2.style.border = "2px solid #a2a3b1";
-  item3.style.border = "2px solid #a2a3b1";
-  item4.style.border = "2px solid #a2a3b1";
+img5.addEventListener("click", () => {
+  imgIndex = 5;
+  orderNum.textContent = "05";
+  img5.classList.add("active");
+  img2.classList.remove("active");
+  img3.classList.remove("active");
+  img4.classList.remove("active");
+  img1.classList.remove("active");
   mainImg.innerHTML = `<img src="img/img6.svg" alt="" />`;
 });
